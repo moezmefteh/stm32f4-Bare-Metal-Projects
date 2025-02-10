6a52c9a2f7c064a790e6fcf58edcb9a401f0639d
moezmefteh <moezmefteh85@gmail.com>
2025-02-10 15:06:00 +0100

    feat: Add UART receiver functionality and LED control

--- projects/UART/uart.c
+++ projects/UART/uart.c
@@ -1,15 +1,40 @@
 #include "uart.h"
 
+char key;
 int main(void)
 {
     uar2_tx_init();
+    init_led_12();
+    uart2_write('Y.............\n\r');
+    printf("Hello from STM32F4.............\n\r");
     while(1)
     {
-        uart2_write('Y.............\n\r');
-        printf("Hello from STM32F4.............\n\r");
+        key = uart2_read();
+        if(key == '1')
+        {
+            GPIOD->ODR |= (1 << LED_PIN_1);
+        }
+        else
+        {
+            GPIOD->ODR &= ~(1 << LED_PIN_1);
+        }
     }
 }
 
+void init_led_12(void)
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN
+    GPIOD->MODER |= GPIO_MODER_MODER12_0;
+    GPIOD->MODER &= ~GPIO_MODER_MODER12_1;
+}
+char uart2_read(void)
+{
+    /*Check the Receiver Data Register is not empty*/
+    /* RXNEIE: RXNE interrupt enable*/
+    while(!(USART2->SR & USART_CR1_RXNEIE)){}
+    /*Read Data*/
+    return USART2->DR;
+}
 int __io_putchar(int ch)
 {
     uart2_write(ch);
@@ -22,7 +47,7 @@ void uart2_write(int ch)
     /*write to transmit data register*/
     USART2->DR = (ch & 0xFF);
 }
-void uar2_tx_init(void)
+void uar2_tx_rx_init(void)
 {
     /******************Configure UART GPIO pins******************/
     /*Enable clock access to GPIOA*/
@@ -36,6 +61,15 @@ void uar2_tx_init(void)
     GPIOA->AFR[0] |= GPIO_AFRL_AFRL2_2
     GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL2_3
 
+    /*Set PA3 mode to alternate function mode in GPIOx_MODER register*/
+    GPIOA->MODER |= GPIO_MODER_MODER3_0
+    GPIOA->MODER &= ~GPIO_MODER_MODER3_1
+    /*set PA3 (AFRL2) alternate function type to UART_TX (AF07) in GPIOx_AFRL register*/
+    GPIOA->AFR[0] |= GPIO_AFRL_AFRL3_0
+    GPIOA->AFR[0] |= GPIO_AFRL_AFRL3_1
+    GPIOA->AFR[0] |= GPIO_AFRL_AFRL3_2
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL3_3
+
     /******************Configure UART Module******************/
     /*Enable clock access to UART2*/
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
@@ -44,6 +78,8 @@ void uar2_tx_init(void)
     /*Configure the transfert direction*/
     USART2->CR1 = USART_CR1_TE;
     USART2->CR1 |= USART_CR1_UE;
+    /*Enable Receiver*/
+    USART2->CR1 |= USART_CR1_RE;
 }
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate)
 {
--- projects/UART/uart.h
+++ projects/UART/uart.h
@@ -8,10 +8,13 @@
 #define SYS_FREQ            16000000
 #define APB1_CLK            SYS_FREQ
 #define UART_BAUDRATE       115200
+#define LED_PIN_1 12
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate);
 static uint16_t compte_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
 void uar2_tx_init(void);
 void uart2_write(int ch);
+char uart2_read(void);
+void init_led_12(void);
 
 #endif
