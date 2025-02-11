59a27d95dbb4860db7c99dbb0f9fa43be4d23d97
moezmefteh <moezmefteh85@gmail.com>
2025-02-11 14:39:28 +0100

    implement ADC peripheral

--- projects/ADC/adc.c
+++ projects/ADC/adc.c
@@ -1,7 +1,46 @@
 #include "adc.h"
+#include "uart.h"
 
+uint32_t sensor_value;
 int main(void)
 {
-
+    pa1_adc_init();
+    start_conversion();
+    while(1){
+        sensor_value = adc_read();
+        printf("sensor value : %d \n\r", sensor_value);
+    }
 }
 
+uint32_t adc_read(void)
+{
+    /*wait for conversion to be completed*/
+    while(!(ADC1->SR & ADC_SR_EOC)){}
+    /*read converted result*/
+    return (ADC1->DR);
+}
+void start_conversion(void)
+{
+    /*start adc conversion*/
+    ADC1->CR2 |= ADC_CR2_SWSTART;
+}
+void pa1_adc_init(void)
+{
+    /******************Configure ADC GPIO pins******************/
+    /*Enable clock access to GPIOA*/
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    /*Set PA1 mode to analog mode in GPIOx_MODER register*/
+    GPIOA->MODER |= GPIO_MODER_MODER1_0
+    GPIOA->MODER |= GPIO_MODER_MODER1_1
+
+    /******************Configure ADC Module******************/
+    /*Enable clock access to ADC*/
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    /*Configure the ADC params*/
+    /*Conversion sequence start*/
+    ADC1->SQR3 = ADC_SQR3_SQ1;
+    /*Conversion sequence length*/
+    ADC1->SQR1 &= ~ADC_SQR1_L;
+    /*Enable ADC module*/
+    ADC1->CR2 |= ADC_CR2_ADON;
+}
--- projects/ADC/adc.h
+++ projects/ADC/adc.h
@@ -2,5 +2,9 @@
 #define __ADC_H__
 
 #include "stm32f4xx.h"
+include <stdint.h>
+uint32_t adc_read(void);
+void start_conversion(void);
+void pa1_adc_init(void);
 
 #endif
