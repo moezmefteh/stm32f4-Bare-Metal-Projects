a970b02f9b32a2e2a1e14baa7d574af44653ea35
moezmefteh <moezmefteh85@gmail.com>
2025-02-11 19:05:33 +0100

    Implement SysTick Driver

--- projects/SysTick/systick.c
+++ projects/SysTick/systick.c
@@ -2,5 +2,29 @@
 
 int main(void)
 {
+    init_led();
+    uar2_tx_rx_init();
+    while(1)
+    {
+        printf("Hello from STM32F4.............\n\r");
+        led_toggle();
+        systickDelayMs(1000);
+    }
+}
 
+void systickDelayMs(int delay)
+{
+    /*Configure systick*/
+    /*Reload with nbr of clocks per millisecond*/
+    SysTick->LOAD = SYSTICK_LOAD_VAL;
+    /*Clear systick current value register*/
+    SysTick->VAL = 0;
+    /*Enable systick and select internal clk src*/
+    Systick->CTRL = SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_CLKSOURCE_Msk;
+
+    for(int i = 0; i < delay; i++){
+        /*wait until the COUNTFLAG is set*/
+        while(!(Systick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)){}
+    }
+    Systick->CTRL = 0;
 }
--- projects/SysTick/systick.h
+++ projects/SysTick/systick.h
@@ -2,6 +2,10 @@
 #define __SYSTICK_H__
 
 #include "stm32f4xx.h"
+#include "uart.h"
+#include "blinky.h"
 
+#define SYSTICK_LOAD_VAL        16000
+void systickDelayMs(int delay);
 
 #endif
