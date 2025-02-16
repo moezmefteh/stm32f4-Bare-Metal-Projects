615882ff3d8ce8eba0f1a2187ddb743fa002b39f
moezmefteh <moezmefteh85@gmail.com>
2025-02-16 01:06:55 +0100

    add syscalls.c for io stream

---
+++ projects/UART/syscalls.c
@@ -0,0 +1,43 @@
+#include <sys/stat.h>
+#include <unistd.h>
+
+int _write(int file, char *ptr, int len) {
+    (void)file;
+    for (int i = 0; i < len; i++) {
+        // Implement your UART send function here
+        uart2_write(ptr[i]);
+    }
+    return len;
+}
+
+int _read(int file, char *ptr, int len) {
+    (void)file;
+    // Implement your UART receive function here
+    for (int i = 0; i < len; i++) {
+        ptr[i] = uart2_read();
+    }
+    return len;
+}
+
+int _close(int file) {
+    (void)file;
+    return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+    (void)file;
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _isatty(int file) {
+    (void)file;
+    return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+    (void)file;
+    (void)ptr;
+    (void)dir;
+    return 0;
+}
