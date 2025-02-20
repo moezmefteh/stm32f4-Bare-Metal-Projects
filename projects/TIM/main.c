b47983886f808222f5a658f99781592678ffd487
moezmefteh <moezmefteh85@gmail.com>
2025-02-20 18:02:28 +0100

    Added TIM4 PD12 output compare function

--- projects/TIM/main.c
+++ projects/TIM/main.c
@@ -5,6 +5,7 @@ int main(void)
     init_led();
     tim2_1hz_init();
     uart2_tx_rx_init();
+    tim4_pd12_output_compare();
     while(1)
     {
         /*wait for UIF*/
@@ -12,6 +13,6 @@ int main(void)
         /*Clear UIF*/
         TIM2->SR &= ~TIM_SR_UIF;
         printf("Hello from STM32F4.............\n\r");
-        led_toggle();
+        //led_toggle();
     }
 }
