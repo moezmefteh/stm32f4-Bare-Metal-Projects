6bd24bfe2b58c2e34b949b94d4ae77011aa623b5
moezmefteh <moezmefteh85@gmail.com>
2025-02-23 00:42:46 +0100

    Implement Timer Input Capture Driver

--- projects/TIM/main.c
+++ projects/TIM/main.c
@@ -1,16 +1,26 @@
 #include "tim.h"
+int timestamp = 0;
 
 int main(void)
 {
     tim2_1hz_init();
     uart2_tx_rx_init();
+    printf("Hello from STM32F4.............\n\r");
     tim4_pd12_output_compare();
+    tim5_pa0_input_capture();
     while(1)
     {
         /*wait for UIF*/
-        while(!(TIM2->SR & TIM_SR_UIF)){}
+        //while(!(TIM2->SR & TIM_SR_UIF)){}
         /*Clear UIF*/
-        TIM2->SR &= ~TIM_SR_UIF;
-        printf("Hello from STM32F4.............\n\r");
+        //TIM2->SR &= ~TIM_SR_UIF;
+        //printf("Hello from STM32F4.............\n\r");
+
+        /*wait until edge is captured*/
+        while(!(TIM5->SR & TIM_SR_CC1IF)){}
+        /*Read captured value*/
+        timestamp = TIM5->CCR1;
+        printf("..........%d\n\r",timestamp);
+
     }
 }
