9ca68e989ed1bb44ac653d2c1e5d5e94d4925ec4
moezmefteh <moezmefteh85@gmail.com>
2025-02-24 16:12:34 +0100

    Add Interrupt APP

---
+++ projects/NVIC/Debug/main.d
@@ -0,0 +1,19 @@
+Debug/main.d: main.c ../../HAL/inc/exti.h \
+ ../../Drivers/CMSIS/Device/stm32f4xx.h \
+ ../../Drivers/CMSIS/Device/stm32f407xx.h \
+ ../../Drivers/CMSIS/Core/core_cm4.h \
+ ../../Drivers/CMSIS/Core/cmsis_version.h \
+ ../../Drivers/CMSIS/Core/cmsis_compiler.h \
+ ../../Drivers/CMSIS/Core/cmsis_gcc.h \
+ ../../Drivers/CMSIS/Core/mpu_armv7.h \
+ ../../Drivers/CMSIS/Device/system_stm32f4xx.h ../../HAL/inc/uart.h
+../../HAL/inc/exti.h:
+../../Drivers/CMSIS/Device/stm32f4xx.h:
+../../Drivers/CMSIS/Device/stm32f407xx.h:
+../../Drivers/CMSIS/Core/core_cm4.h:
+../../Drivers/CMSIS/Core/cmsis_version.h:
+../../Drivers/CMSIS/Core/cmsis_compiler.h:
+../../Drivers/CMSIS/Core/cmsis_gcc.h:
+../../Drivers/CMSIS/Core/mpu_armv7.h:
+../../Drivers/CMSIS/Device/system_stm32f4xx.h:
+../../HAL/inc/uart.h:
---
+++ projects/NVIC/Debug/stm32_startup_f4.d
@@ -0,0 +1,1 @@
+Debug/stm32_startup_f4.d: ../../Startup/stm32_startup_f4.c
