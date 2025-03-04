e148f4531bd72f2cd85a0f5b8c8ac985a829212d
moezmefteh <moezmefteh85@gmail.com>
2025-03-04 10:55:34 +0100

    Setup: Create folders and files for DMA application

---
+++ HAL/inc/dma.h
@@ -0,0 +1,9 @@
+#ifndef __DMA_H__
+#define __DMA_H__
+
+#include "stm32f4xx.h"
+#include <stdio.h>
+
+extern void uart2_tx_rx_init(void);
+
+#endif
---
+++ projects/DMA/main.c
@@ -0,0 +1,10 @@
+#include "dma.h"
+
+int main(void)
+{
+    uart2_tx_rx_init();
+    printf("Hello from STM32F4.............\n\r");
+    while(1){
+
+    }
+}
