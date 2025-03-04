1630565686f91607a9db4409c31e834635281217
moezmefteh <moezmefteh85@gmail.com>
2025-03-04 17:08:33 +0100

    Add DMA UART interrupt app

--- projects/DMA/main.c
+++ projects/DMA/main.c
@@ -2,8 +2,12 @@
 
 int main(void)
 {
+    char message[31] = "Hello from stm32 DMA transfer\n\r";
     uart2_tx_rx_init();
     printf("Hello from STM32F4.............\n\r");
+    init_led();
+    dma1_stream6_init((uint32_t) message, (uint32_t) &USART2->DR, 31);
+
     while(1){
 
     }
